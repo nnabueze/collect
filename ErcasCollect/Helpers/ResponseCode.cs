0b2edd1180301177e39e3e32c45f384b0d070635
Nnabueze Opara <villavelle101@gmail.com>
2021-01-19 12:28:31 +0100

    added response class

---
+++ ErcasCollect/Helpers/ResponseCode.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ErcasCollect.Helpers
+{
+    public class ResponseCode
+    {
+        public int OK { get; set; }
+
+        public int Created { get; set; }
+
+        public int NoContent { get; set; }
+
+        public int TransactionSuccessful { get; set; }
+
+        public int AccountActivated { get; set; }
+
+        public int RemmitanceGenerated { get; set; }
+
+        public int POSActivated { get; set; }
+
+        public int BadRequest { get; set; }
+
+        public int Unauthorized { get; set; }
+
+        public int NotFound { get; set; }
+
+        public int TransactionFailed { get; set; }
+
+        public int TransactionRejected { get; set; }
+
+        public int InvalidPIN { get; set; }
+
+        public int TransactionProcessing { get; set; }
+
+        public int TransactionReversed { get; set; }
+
+        public int AccountSuspended { get; set; }
+
+        public int POSDeactivated { get; set; }
+
+        public int PendingTaxes { get; set; }
+
+        public int ClearedTaxes { get; set; }
+    }
+}
