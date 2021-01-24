00f6dd622e480d2c548ada78680e28e9ded0a533
Nnabueze Opara <villavelle101@gmail.com>
2021-01-24 03:30:12 +0100

    removed xml serizer

--- ErcasCollect/Helpers/JsonXmlObjectConverter.cs
+++ ErcasCollect/Helpers/JsonXmlObjectConverter.cs
@@ -1,11 +1,59 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
+using System.Xml.Serialization;
 
 namespace ErcasCollect.Helpers
 {
     public class JsonXmlObjectConverter
     {
-    }
+        public class XmlObjectConverter
+        {
+            public static string SerializeObject<T>(T dataObject)
+            {
+                if (dataObject == null)
+                {
+                    return string.Empty;
+                }
+                try
+                {
+                    using (StringWriter stringWriter = new System.IO.StringWriter())
+                    {
+                        var serializer = new XmlSerializer(typeof(T));
+                        serializer.Serialize(stringWriter, dataObject);
+                        string x = stringWriter.ToString();
+                        string y = x.Replace("xmlns:xsd=\"http://www.w3.org/2001/XMLSchema\"", "");
+                        string z = y.Replace("xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\"", "");
+                        return z;
+                    }
+                }
+                catch (Exception ex)
+                {
+                    return ex.Message.ToString();
+                }
+            }
+
+            public static T DeserializeObject<T>(string xml)
+                 where T : new()
+            {
+                if (string.IsNullOrEmpty(xml))
+                {
+                    return new T();
+                }
+                try
+                {
+                    using (var stringReader = new StringReader(xml))
+                    {
+                        var serializer = new XmlSerializer(typeof(T));
+                        return (T)serializer.Deserialize(stringReader);
+                    }
+                }
+                catch (Exception)
+                {
+                    return new T();
+                }
+            }
+        }
 }
