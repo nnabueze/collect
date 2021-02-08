e459e1eceb3d6a88f01199302509f08e752ed456
Nnabueze Opara <villavelle101@gmail.com>
2021-02-08 12:39:22 +0100

    adding invoice

---
+++ ErcasCollect/Commands/Collection/PosInvoiceCommand.cs
@@ -0,0 +1,57 @@
+﻿using AutoMapper;
+using ErcasCollect.Commands.Dto.CollectionDto;
+using ErcasCollect.Domain.Interfaces;
+using ErcasCollect.Domain.Models;
+using ErcasCollect.Helpers;
+using ErcasCollect.Responses;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ErcasCollect.Commands.Collection
+{
+    public class PosInvoiceCommand : IRequest<SuccessfulResponse>
+    {
+        public PosInvoiceDto posInvoiceDto { get; set; }
+
+        public class PosInvoiceCommandHandler : IRequestHandler<PosInvoiceCommand, SuccessfulResponse>
+        {
+            private readonly ResponseCode _responseCode;
+
+            private readonly IGenericRepository<Transaction> _transactionRepository;
+
+            private readonly IGenericRepository<Batch> _BatchRepository;
+
+            private readonly IGenericRepository<Pos> _posRespository;
+
+            private readonly IGenericRepository<User> _userRepository;
+
+            private readonly IMapper _mapper;
+
+            private readonly IGenericRepository<CategoryTwoService> _categoryTwoServiceRepository;
+
+            private readonly IGenericRepository<LevelTwo> _levelTwoRepository;
+
+            private readonly IGenericRepository<CloseBatchTransaction> _closeBatchTransactionRepository;
+
+            public Task<SuccessfulResponse> Handle(PosInvoiceCommand request, CancellationToken cancellationToken)
+            {
+                //Get and check level two
+
+                //add batch tranasction
+
+                //add trabsaction
+
+                //add close batch
+
+                //send email
+
+                //retyrn response
+                return null;
+            }
+        }
+    }
+}
---
+++ ErcasCollect/Commands/Dto/CollectionDto/PosInvoiceDto.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ErcasCollect.Commands.Dto.CollectionDto
+{
+    public class PosInvoiceDto
+    {
+        public string PosId { get; set; }
+
+        public string UserId { get; set; }
+
+        public string BillerId { get; set; }
+
+        public string LevelOneId { get; set; }
+
+        public string LevelTwoId { get; set; }
+
+        public string TotalAmount { get; set; }
+
+        public List<InvoiceDetail> Invoices { get; set; }
+    }
+
+    public class InvoiceDetail
+    {
+        public string PayerName { get; set; }
+
+        public string Amount { get; set; }
+
+        public string PayerPhone { get; set; }
+
+        public string CategoryTwoId { get; set; }
+    }
+}
