9216a740f4054667799ba3de183c7cb5501e8d18
Nnabueze Opara <villavelle101@gmail.com>
2021-09-30 12:13:04 +0100

    added container migration

--- ErcasCollect/Helpers/PrepMigration.cs
+++ ErcasCollect/Helpers/PrepMigration.cs
@@ -1,11 +1,29 @@
-﻿using System;
+﻿using ErcasCollect.DataAccess;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace ErcasCollect.Helpers
 {
-    public class PrepMigration
+    public static class PrepMigration
     {
+        public static void PrepPopulation(IApplicationBuilder app)
+        {
+            using (var serviceScope = app.ApplicationServices.CreateScope())
+            {
+                Populate(serviceScope.ServiceProvider.GetService<ApplicationDbContext>());
+            }
+        }
+
+        public static void Populate(ApplicationDbContext context)
+        {
+            Console.WriteLine("Applying Migration");
+
+            context.Database.Migrate();
+        }
     }
 }
